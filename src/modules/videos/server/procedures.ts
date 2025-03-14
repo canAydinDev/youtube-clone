24b55477a27c4a22e9f12bd6b03e1548358fc36d
canaydindev <canaydindev@gmail.com>
2025-03-14 22:45:24 +0300

    webhook finished

--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -1,11 +1,31 @@
 import { db } from "@/db";
 import { videos } from "@/db/schema";
+import { mux } from "@/lib/mux";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 //import { TRPCError } from "@trpc/server";
 
 export const videosRouter = createTRPCRouter({
   create: protectedProcedure.mutation(async ({ ctx }) => {
     const { id: userId } = ctx.user;
+    const upload = await mux.video.uploads.create({
+      new_asset_settings: {
+        passthrough: userId,
+        playback_policy: ["public"],
+        input: [
+          {
+            generated_subtitles: [
+              {
+                language_code: "en",
+                name: "English",
+              },
+            ],
+          },
+        ],
+      },
+      cors_origin: "*", //TODO: In production, set to your url
+    });
+    console.log({ upload });
+    console.log({ url: upload.url });
 
     // throw new TRPCError({
     //   code: "BAD_REQUEST",
@@ -17,11 +37,14 @@ export const videosRouter = createTRPCRouter({
       .values({
         userId,
         title: "Untitled",
+        muxStatus: "waiting",
+        muxUploadId: upload.id,
       })
       .returning();
 
     return {
       video: video,
+      url: upload.url,
     };
   }),
 });
